c2cbef0c408e7926c4b210efbde87b92ae428071
jorektheglitch <lozcontrariuz@yandex.ru>
2021-06-13 07:58:27 +0300

    Add aiohttp in requirements

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jorektheglitch/aioservertiming",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'aiohttp'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
